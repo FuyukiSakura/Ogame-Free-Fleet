a2cdfb05757e422c092bc36388c1a8c14c716222
Eric Lam <ericlamyauping@gmail.com>
2018-07-13 00:49:38 +1000

    Add browser and navigation bars

--- FreeFleet/FreeFleet.UWP/ViewModels/GamePageViewModel.cs
+++ FreeFleet/FreeFleet.UWP/ViewModels/GamePageViewModel.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FreeFleet.ViewModels;
 
 namespace FreeFleet.UWP.ViewModels
 {
-    public class GamePageViewModel
+    public class GamePageViewModel : BindableBase
     {
+        private string _mainUrl;
 
+        #region Auto Properties
+
+        public string MainUrl
+        {
+            get => _mainUrl;
+            set
+            {
+                _mainUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
     }
 }
--- FreeFleet/FreeFleet.UWP/Views/GamePage.xaml.cs
+++ FreeFleet/FreeFleet.UWP/Views/GamePage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FreeFleet.UWP.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,13 +23,69 @@ namespace FreeFleet.UWP.Views
     /// </summary>
     public sealed partial class GamePage : Page
     {
+        private GamePageViewModel _vm;
+
         public GamePage()
         {
             this.InitializeComponent();
+            _vm = (GamePageViewModel) DataContext;
+        }
+
+        #region Buttons
+
+        /// <summary>
+        /// Handles refresh button on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void RefreshBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            GameBrowser.Refresh();
         }
 
         /// <summary>
-        /// Handles browser navigation complated
+        /// Handles back button on clicked
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NavigateBackBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (GameBrowser.CanGoBack)
+            {
+                GameBrowser.GoBack();
+            }
+        }
+
+        /// <summary>
+        /// Handles forward button on click
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NavigateForwardBtn_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (GameBrowser.CanGoForward)
+            {
+                GameBrowser.GoForward();
+            }
+        }
+
+        #endregion
+
+
+        #region Browser Handlers
+
+        /// <summary>
+        /// Handles when browser starts navigating
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void GameBrowser_OnNavigationStarting(WebView sender, WebViewNavigationStartingEventArgs args)
+        {
+            _vm.MainUrl = args.Uri.AbsoluteUri;
+        }
+
+        /// <summary>
+        /// Handles browser navigation completed
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="args"></param>
@@ -37,7 +94,24 @@ namespace FreeFleet.UWP.Views
             if (args.Uri.Host == "lobby.ogame.gameforge.com")
             {
                 // if in lobby, get accounts
+                Console.WriteLine("In lobby...");
             }
         }
+
+        #endregion
+
+        #region Overrides
+
+        /// <summary>
+        /// Handles when the page become active
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        { 
+            // Load URI after the page is loaded
+            GameBrowser.Source = new Uri("https://tw.ogame.gameforge.com");
+        }
+
+        #endregion
     }
 }
