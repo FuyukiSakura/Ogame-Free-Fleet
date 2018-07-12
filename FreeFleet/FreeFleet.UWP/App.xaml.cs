efff99e16ac0092085dab4eca864caaaee6329ee
Eric Lam <ericlamyauping@gmail.com>
2018-07-13 00:12:43 +1000

    use Native UWP

--- FreeFleet/FreeFleet.UWP/App.xaml.cs
+++ FreeFleet/FreeFleet.UWP/App.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using FreeFleet.UWP.Views;
 
 namespace FreeFleet.UWP
 {
@@ -68,7 +69,7 @@ namespace FreeFleet.UWP
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                rootFrame.Navigate(typeof(GamePage), e.Arguments);
             }
             // Ensure the current window is active
             Window.Current.Activate();
