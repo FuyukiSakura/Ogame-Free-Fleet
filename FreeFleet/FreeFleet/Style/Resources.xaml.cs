6d0c1410c662eae8c5c65c188cffc9d11f0a2f5e
Eric Lam <ericlamyauping@gmail.com>
2018-07-15 17:47:08 +1000

    add font awesome

---
+++ FreeFleet/FreeFleet/Style/Resources.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FreeFleet.Style
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resources : ResourceDictionary
+    {
+        public Resources()
+        {
+            InitializeComponent();
+        }
+    }
+}
