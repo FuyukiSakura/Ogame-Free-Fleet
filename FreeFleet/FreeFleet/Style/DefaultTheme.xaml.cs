5f97d7a2c8911ce5e5af562c626965726fdba7a1
Eric Lam <ericlamyauping@gmail.com>
2018-07-15 13:54:52 +1000

    fix release mode crash

---
+++ FreeFleet/FreeFleet/Style/DefaultTheme.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FreeFleet.Style
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultTheme : ResourceDictionary
+    {
+        public DefaultTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
