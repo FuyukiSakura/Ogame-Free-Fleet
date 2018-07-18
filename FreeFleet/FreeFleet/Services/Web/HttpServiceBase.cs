13b9a7d46d95659f822f7061c9892bd8f841bdcd
Eric Lam <ericlamyauping@gmail.com>
2018-07-18 23:48:01 +1000

    prevent webrequest errors causing app to crash

--- FreeFleet/FreeFleet/Services/Web/HttpServiceBase.cs
+++ FreeFleet/FreeFleet/Services/Web/HttpServiceBase.cs
@@ -68,52 +68,64 @@ namespace FreeFleet.Services.Web
             // Load Event Fleets page
             var uri = new Uri(string.Format(UriList.OgameEventFleetUrl,
                 host));
-            var response = await HttpSendRequestAsync(uri);
-            if (!response.IsSuccessStatusCode)
+            try
             {
-                // Failed requesting resources
-                Logger.Log(string.Format(DebugMessage.EventFleetRefreshFail, 
-                    DebugMessage.EventFleetRefreshRejected, 
-                    ""));
-                return null;
-            }
-
-            // Check if logged out
-            if (response.RequestMessage.RequestUri.Host == UriList.OgameLobbyHost){
-                // Logged out return
-                Logger.Log(string.Format(DebugMessage.EventFleetRefreshFail,
-                    DebugMessage.EventFleetRefreshLoggedOut,
-                    ""));
-                return null;
-            }
-
-            var doc = new HtmlDocument();
-            doc.LoadHtml(await response.Content.ReadAsStringAsync());
-
-            // Parse event fleet page into Event Fleet details
-            var fleets = new List<EventFleet>();
-            var eventFleetNodes = doc.DocumentNode.SelectNodes("//tr[@class='eventFleet']");
-            if (eventFleetNodes == null) {
-                Logger.Log(DebugMessage.EventFleetRefreshEmptyFleet);
-                return fleets.ToArray(); // No event, return
-            }
-
-            fleets.AddRange(from node in eventFleetNodes
-                let coordOrigin = node.SelectSingleNode("./td[@class='coordsOrigin']/a")?.InnerHtml.Trim() ?? SharedResources.NodeNotFound
-                let coordDest = node.SelectSingleNode("./td[@class='destCoords']/a")?.InnerHtml.Trim() ?? SharedResources.NodeNotFound
-                let detailsFleet = node.SelectSingleNode("./td[@class='detailsFleet']/span")?.InnerHtml ?? "0"
-                let missionType = node.Attributes["data-mission-type"]?.Value ?? "1"
-                select new EventFleet
+                var response = await HttpSendRequestAsync(uri);
+                if (!response.IsSuccessStatusCode)
                 {
-                    Id = node.Id,
-                    CoordsOrigin = coordOrigin,
-                    CoordsDest = coordDest,
-                    DetailsFleet = Convert.ToInt64(detailsFleet),
-                    MissionType = (MissionType) Convert.ToInt16(missionType)
-                });
+                    // Failed requesting resources
+                    Logger.Log(string.Format(DebugMessage.EventFleetRefreshFail,
+                        DebugMessage.EventFleetRefreshRejected,
+                        ""));
+                    return null;
+                }
 
-            Logger.Log(DebugMessage.EventFleetRefreshSuccess);
-            return fleets.ToArray();
+                // Check if logged out
+                if (response.RequestMessage.RequestUri.Host == UriList.OgameLobbyHost)
+                {
+                    // Logged out return
+                    Logger.Log(string.Format(DebugMessage.EventFleetRefreshFail,
+                        DebugMessage.EventFleetRefreshLoggedOut,
+                        ""));
+                    return null;
+                }
+
+                var doc = new HtmlDocument();
+                doc.LoadHtml(await response.Content.ReadAsStringAsync());
+
+                // Parse event fleet page into Event Fleet details
+                var fleets = new List<EventFleet>();
+                var eventFleetNodes = doc.DocumentNode.SelectNodes("//tr[@class='eventFleet']");
+                if (eventFleetNodes == null)
+                {
+                    Logger.Log(DebugMessage.EventFleetRefreshEmptyFleet);
+                    return fleets.ToArray(); // No event, return
+                }
+
+                fleets.AddRange(from node in eventFleetNodes
+                    let coordOrigin = node.SelectSingleNode("./td[@class='coordsOrigin']/a")?.InnerHtml.Trim() ??
+                                      SharedResources.NodeNotFound
+                    let coordDest = node.SelectSingleNode("./td[@class='destCoords']/a")?.InnerHtml.Trim() ??
+                                    SharedResources.NodeNotFound
+                    let detailsFleet = node.SelectSingleNode("./td[@class='detailsFleet']/span")?.InnerHtml ?? "0"
+                    let missionType = node.Attributes["data-mission-type"]?.Value ?? "1"
+                    select new EventFleet
+                    {
+                        Id = node.Id,
+                        CoordsOrigin = coordOrigin,
+                        CoordsDest = coordDest,
+                        DetailsFleet = Convert.ToInt64(detailsFleet),
+                        MissionType = (MissionType) Convert.ToInt16(missionType)
+                    });
+
+                Logger.Log(DebugMessage.EventFleetRefreshSuccess);
+                return fleets.ToArray();
+            }
+            catch (HttpRequestException ex)
+            {
+                Logger.Log(ex.Message);
+                return null;
+            }
         }
 
         #endregion
